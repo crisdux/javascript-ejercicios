c725b95034ac703751d452fda6fcde7e6a7d2a43
crisdux <crisfer.4217@gmail.com>
2023-11-04 18:23:29 -0400

    ejercicio 107 arreglado

--- 107-ejercicio.js
+++ 107-ejercicio.js
@@ -1,8 +1,8 @@
 // contar la cantidad del caracter en la cedena
-const contarCaracteres = (cadena, caracter) => {
-  let contador = 0;
-  return [...cadena].filter((palabra) => palabra.includes(caracter)).length;
-};
-
-console.log(contarCaracteres("Hola, mundo en JavaScript", "o")); //2
-console.log(contarCaracteres("Javascript es genial", "p")); // 1
+const contarCaracteres  = (cadena, caracter) => {
+    let cadenaRes = cadena.toLowerCase().split(" ")
+    return cadenaRes.filter((palabra) => palabra.includes(caracter)).length
+  };
+  
+  console.log(contarCaracteres ("Hola, mundo en JavaScript", "j")); //2
+  console.log(contarCaracteres ("Javascript es genial", "p")); // 1
