cb1d1db95ac575d7fabe74ddea586a4a1188a535
crisdux <crisfer.4217@gmail.com>
2023-11-18 00:05:25 -0400

    ejercicio 182

---
+++ 182-ejercicio.js
@@ -0,0 +1,16 @@
+/**
+ * @name filterNumbersGreaterThan
+ * @description Filtra los números de un array que sean mayor a cierto número x dejando solo los que sean menores a este
+ *
+ * @param {number[]} numbers - Array de números a filtrar
+ * @param {number} filter - Número a partir del cuál filtrar los demás números
+ * @returns {Number[]} - Array de números filtrados menores a {filter}
+ *
+ * @example
+ *  filterNumbersGreaterThan([3, 8, 12, 1, 7, 4], 7) // returns [3, 1, 4]
+ */
+
+const filterNumbersGreaterThan = (numbers, filter) => {
+    return numbers.filter((number) => number<filter)
+}
+console.log(filterNumbersGreaterThan([3, 8, 12, 1, 7, 4], 7))
